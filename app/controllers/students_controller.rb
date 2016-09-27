30f267360f593f735ac25cd028249a073cae8fad
John Gill <j.gill0431@gmail.com>
2016-09-27 12:24:09 -0700

    Working on student and cohort views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,19 @@
 class StudentsController < ApplicationController
   def index
+    p params
+    @cohort = Cohort.find(params[:cohort_id])
+    @students = @cohort.students
   end
+
   def show
   end
 
   def edit
+    if params[:student][:enrollment]
+      p "ENROLLMENT CHANGE"
+    else
+      # NORMAL EDIT FUNCTION HAPPENS
+    end
   end
 
   def delete
