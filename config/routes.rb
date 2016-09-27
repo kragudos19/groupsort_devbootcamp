087a16daf1dd768a3c76887e5ed386cbf4da9cd4
John Gill <j.gill0431@gmail.com>
2016-09-27 11:10:25 -0700

    Create views in slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,30 @@
 Rails.application.routes.draw do
-
-  root to: 'cohorts#index'
-
-  resources :cohorts do
-    resources :phases, only: [:new, :create, :index]
-    resources :students, only: [:new, :create, :index]
-  end
-
-  resource :phases, only: [:show, :edit, :delete, :update] do
-    resources :weeks, only: [:new, :create, :index]
-  end
-
-  resources :weeks, only: [:show, :edit, :delete, :update] do
-    resources :groups, only: [:new, :create, :index]
-  end
-
-  resources :students, only: [:show, :edit, :delete, :update] do
-    resources :groups, only: [:index]
-  end
-
-  resource :groups, only: [:show, :edit, :delete, :update]
-
   devise_for :users
 
+  authenticated :user do
+    root 'cohorts#index', as: :authenticated_root
+
+
+    resources :cohorts do
+      resources :phases, only: [:new, :create, :index]
+      resources :students, only: [:new, :create, :index]
+    end
+
+    resource :phases, only: [:show, :edit, :delete, :update] do
+      resources :weeks, only: [:new, :create, :index]
+    end
+
+    resources :weeks, only: [:show, :edit, :delete, :update] do
+      resources :groups, only: [:new, :create, :index]
+    end
+
+    resources :students, only: [:show, :edit, :delete, :update] do
+      resources :groups, only: [:index]
+    end
+
+    resource :groups, only: [:show, :edit, :delete, :update]
+  end
+
+
+  root "home#index"
 end
