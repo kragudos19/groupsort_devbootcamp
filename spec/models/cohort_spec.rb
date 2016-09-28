9b188c44c7d558193581e68ed3260b6c2e30070a
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-09-28 11:56:26 -0700

    Generated spec directory and added the Cohort model test to it

---
+++ spec/models/cohort_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe Cohort do
+  context "when displaying the cohort's name" do 
+    it "is blah blah blah"
+      cohort = Cohort.new(name: "Kangaroos")
+      expect(cohort.name).to eq("Kangaroos")
+    end
+  end
+end
