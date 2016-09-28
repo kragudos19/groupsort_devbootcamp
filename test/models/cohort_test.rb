60a1213837101e00989ef0c73f71644a2685867d
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-09-28 11:52:05 -0700

    Add .travis.yml and rspec gem, plus skeleton of basic test on Cohort model

--- test/models/cohort_test.rb
+++ test/models/cohort_test.rb
@@ -1,7 +1,18 @@
-require 'test_helper'
+# require 'test_helper'
 
-class CohortTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+# class CohortTest < ActiveSupport::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
+
+require "spec_helper"
+
+describe Cohort do
+  context "when displaying the cohort's name" do 
+    it "is blah blah blah"
+      cohort = Cohort.new(name: "Kangaroos")
+      expect(cohort.name).to eq("Kangaroos")
+    end
+  end
 end
