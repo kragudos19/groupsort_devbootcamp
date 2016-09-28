e6fb90946b7bffa2ea22ef4190c33785899b2e6a
John Gill <j.gill0431@gmail.com>
2016-09-28 16:07:01 -0700

    FIX PASSWORD ISSUE

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => 'smtp.sendgrid.net',
   :port                 =>  '587',
   :authentication       =>  :plain,
-  :user_name            =>  'app57174622@heroku.com',
-  :password             =>  'vypjfxhk1980',
+  :user_name            =>  ENV['SENDGRID_USERNAME'],
+  :password             =>  ENV['SENDGRID_PASSWORD'],
   :domain               =>  'heroku.com',
   :enable_starttls_auto =>  true
 }
