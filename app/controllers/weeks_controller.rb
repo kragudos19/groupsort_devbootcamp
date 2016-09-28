8eb722ee8fdfe57a12cbd988264a64d3ec377ae2
Andrew Palmes <email": null>
2016-09-27 20:59:14 -0700

    view added to weeks/:id. update week path: 'update_week_path'

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,16 +1,30 @@
 class WeeksController < ApplicationController
   def show
+    @week = Week.find(params[:id])
   end
 
   def edit
+    @week = Week.find(params[:id])
   end
 
   def delete
   end
 
   def new
+    @week = Week.new
   end
 
   def create
+    @week = Week.new(week_params)
+    if(@week.save)
+
+    end
+  end
+
+  def update
+    #need to implement logic into controller
+    #how will we save table?
+    @week = Week.find(params[:id])
+
   end
 end
