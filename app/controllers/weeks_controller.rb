3685793436054ee868587d5a3defb646a7359946
Ezra Chiang <email": null>
2016-09-28 16:12:10 -0700

    Revisit Update method for Weeks_controller

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -23,28 +23,36 @@ class WeeksController < ApplicationController
 
   def update
 
-    @week = Week.find(params[:id])
-    p @groups = @week.groups
-    p @student_groups = @week.student_groups
-    p "-------------"
-    p params[:groupsData].values
-    p "------------"
+    # @week = Week.find(params[:id])
+    # p @groups = @week.groups
+    # p @student_groups = @week.student_groups
+    # p "-------------"
+    # data = params[:groupsData].values
+    # p "------------"
 
-    for i in 0..params[:length].to_i-1
-      p params[:groupsData][i.to_s]
-      g = Group.find(params[:groupsData][i.to_s][:group])
+    # # data.each do |groups|
+    # #   groups[:students].each do |id|
+    # #     student = Student.find(id)
+    # #     student.student_groups.group_id = groups[:group]
+    # #   end
+    # # end
 
-      for j in 0..params[:groupsData][i.to_s][:students].size-1
-        # #student exists in group already
-        s = StudentGroup.find_by(student_id: params[:groupsData][i.to_s][:students][j].to_i, group_id: params[:groupsData][i.to_s][:group].to_i)
-        if(s)
-          puts "student found!!!"
-        # else
-        #   StudentGroup.create(student_id: params[:key => "value", groupsData][i.to_s][:students][j].to_i, group_id: params[:groupsData][i.to_s][:group].to_i)
-        end
-        # #student has been added from group
-        # #student has been removed from group
-      end
+    # for i in 0..params[:length].to_i-1
+    #   p params[:groupsData][i.to_s]
+    #   g = Group.find(params[:groupsData][i.to_s][:group])
+
+    #   for j in 0..params[:groupsData][i.to_s][:students].size-1
+    #     # #student exists in group already
+    #     s = StudentGroup.find_by(student_id: params[:groupsData][i.to_s][:students][j].to_i, group_id: params[:groupsData][i.to_s][:group].to_i)
+    #     if(s)
+    #       puts "student found!!!"
+    #     else
+    #       # @week.
+    #       # StudentGroup.find_by(student_id: params[:groupsData][i.to_s][:students][j].to_i, group_id: params[:groupsData][i.to_s][:group].to_i)
+    #     end
+    #     # #student has been added from group
+    #     # #student has been removed from group
+    #   end
 
     end
 
@@ -57,5 +65,5 @@ class WeeksController < ApplicationController
     #need to implement logic into controller
     #how will we save table?
 
-  end
+  # end
 end
