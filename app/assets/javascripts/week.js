9ed275ef27e3be48d1ba7104adef39e65adae015
Ezra Chiang <email": null>
2016-09-28 11:13:38 -0700

    Add draggable and droppable functionality for students in weeks

---
+++ app/assets/javascripts/week.js
@@ -0,0 +1,27 @@
+var ready = function() {
+
+  dragAndDrop();
+
+};
+
+function dragAndDrop(){
+  var student = $(".student").draggable({
+    helper: 'clone'
+  });
+
+  $(".group").droppable({
+    drop: function(event, ui){
+      $(this).append(ui.draggable);
+      console.log("drop to row")
+    }
+  })
+}
+
+
+
+
+
+
+$(document).on('turbolinks:load', ready);
+
+
